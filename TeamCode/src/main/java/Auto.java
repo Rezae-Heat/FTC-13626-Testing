6723eb1a1fe7930aefdb66398c4bff1c7ab2260a
DW09877 <danielwon877@gmail.com>
2023-11-04 13:25:11 +1000

    Created an op mode for the autonomous phase. The created op mode is intended to simply drive the robot around 50 cm to the right and then terminate.

---
+++ TeamCode/src/main/java/Auto.java
@@ -0,0 +1,52 @@
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class Auto extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        DcMotor leftRear = hardwareMap.get(DcMotor.class, "leftRear");
+        DcMotor rightRear = hardwareMap.get(DcMotor.class, "rightRear");
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        leftFront.setPower(0.5);
+        leftRear.setPower(-0.5);
+        rightFront.setPower(-0.5);
+        rightRear.setPower(0.5);
+
+        double t = 2000;  // a parameter (in ms) to be optimized through trial and error.
+        ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+
+        telemetry.addData("Status", "Running");
+        telemetry.update();
+
+        while (timer.time() < t) {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+        }
+
+        leftFront.setPower(0);
+        leftRear.setPower(0);
+        rightFront.setPower(0);
+        rightRear.setPower(0);
+
+        telemetry.addData("Status", "Terminated");
+        telemetry.update();
+    }
+}
